adad9817e4743b21c7e03beb2da5e95ac43c4135
Shockley Lee <shockleylee@gmail.com>
2013-04-11 20:44:44 +0800

    THIS IS THE FIRST IMPORT
    THIS IS THE FIRST IMPORT

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsinstallerDemo::Application.config.secret_token = '61dd58163b99f81e737358e114cf375a03d25d6a07a999c0a5274840b953d503eaa7ddc9075044e8db40a7f8c9c7bb796ae86083b74ed1eb0fcffd12a059b5de'
